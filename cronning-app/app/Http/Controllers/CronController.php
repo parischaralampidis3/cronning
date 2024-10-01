111119d59681622d4eb767d59030dd8eed7496d4
@parou.char <curlycode@outlook.com>
2024-10-01 16:30:10 +0300

    continue at modifying controller

--- cronning-app/app/Http/Controllers/CronController.php
+++ cronning-app/app/Http/Controllers/CronController.php
@@ -10,17 +10,27 @@ class CronController extends Controller
 
     public function index(){
         $crons = Cron::latest()->get();
+        if(!$crons){
+            return response()->json(['message' => 'Cron records are not found.'],404);
+        }
         return response()->json(['crons'=>$crons], 200);
     }
 
     public function show($id){
+        
         $cron = Cron::findOrFail($id);
-         return response()->json(['cron'=>$cron], 200);
+        if(!$cron){
+            return response()->json(['message'=>'Cron record not found'],404);
+        }
+
+        return response()->json(['cron'=>$cron], 200);
     }
 
-    public function store($id){
-        $storeCron = Cron::findOrFail($id);
-
+    public function store(Request $request){
+        $request -> validate([
+            'title'=> 'required|max:255',
+            'script' => 'required|max:255'
+        ]);
     }
     
 }
