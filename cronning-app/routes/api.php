31fdb9499ade2e7e1f1adb52176aa1877814c03e
@parou.char <curlycode@outlook.com>
2024-10-01 15:35:38 +0300

    setting routes

---
+++ cronning-app/routes/api.php
@@ -0,0 +1,12 @@
+<?php 
+use \App\Http\Controllers\CronController;
+
+
+//index route
+Route::get('index',[CronController::class,'index'])->name('index');
+
+//show route
+Route::get('show/{cronner:id}',[CronController::class,'show'])->name('show');
+
+//create route
+Route::post('store',[CronController::class,'store'])->name('store');
